99c7f86c05c520fff0317851ddef2af9175d3493
KhaledSaiidi <khaled.saiidi@outlook.com>
2026-03-14 17:09:39 +0100

    feat: add initial Terraform configuration for Azure infrastructure & vnet resources

---
+++ 6-production-grade-Azure-infra/vnet.tf
@@ -0,0 +1,161 @@
+resource "random_pet" "lb_hostname" {
+
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "${var.environment}-rg"
+  location = var.allowed_locations[0]
+  tags = var.resource_tags
+} 
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "${var.environment}-vnet"
+  address_space       = var.network_config[0]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  tags                = var.resource_tags
+}
+
+resource "azurem_network_security_group" "nsg" {
+  name                = "${var.environment}-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  tags                = var.resource_tags
+
+  security_rule {
+    name                       = "allow-http"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+  security_rule {
+    name                       = "allow-https"
+    priority                   = 101
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+    security_rule {
+    name                       = "allow-ssh"
+    priority                   = 102
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "nsg_assoc" {
+  subnet_id                 = azurerm_virtual_network.vnet.subnet[0].id
+  network_security_group_id = azurem_network_security_group.nsg.id
+}
+
+resource "azurerm_public_ip" "lb_ip" {
+  name                = "${var.environment}-lb-ip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku = "Standard"
+  zones = ["1", "2", "3"]
+  domain_name_label = "${var.environment}-lb-${random_pet.lb_hostname.id}"
+  tags                = var.resource_tags
+}
+
+resource "azurerm_lb" "lb" {
+    name                = "${var.environment}-lb"
+    location            = azurerm_resource_group.rg.location
+    resource_group_name = azurerm_resource_group.rg.name
+    sku                 = "Standard"
+    frontend_ip_configuration {
+        name                 = "LoadBalancerFrontEnd"
+        public_ip_address_id = azurerm_public_ip.lb_ip.id
+    }
+    tags                = var.resource_tags
+}
+
+resource "azurerm_lb_backend_address_pool" "backend_pool" {
+    name                = "${var.environment}-backend-pool"
+    loadbalancer_id     = azurerm_lb.lb.id
+}
+
+resource "azurerm_lb_rule" "lb_rule" {
+    name                           = "${var.environment}-lb-http-rule"
+    loadbalancer_id                 = azurerm_lb.lb.id
+    protocol                       = "Tcp"
+    frontend_port                   = 80
+    backend_port                    = 80
+    frontend_ip_configuration_name = "LoadBalancerFrontEnd"
+    backend_address_pool_ids         = [azurerm_lb_backend_address_pool.backend_pool.id]
+    probe_id = azurerm_lb_probe.lb_probe.id
+    idle_timeout_in_minutes         = 4
+}
+
+resource "azurerm_lb_probe" "lb_probe" {
+    name                = "${var.environment}-lb-http-probe"
+    loadbalancer_id     = azurerm_lb.lb.id
+    protocol            = "Http"
+    port                = 80
+    interval_in_seconds = 5
+    number_of_probes   = 2
+    request_path = "/"
+}
+
+resource "azurerm_lb_nat_rule" "ssh_lb_nat_rule" {
+    name = "${var.environment}-lb-ssh-nat-rule"
+    resource_group_name = "${azurerm_resource_group.rg.name}"
+    loadbalancer_id = azurerm_lb.lb.id
+    protocol = "Tcp"
+    frontend_port = 50000
+    frontend_port_end = 50119
+    backend_port = 22
+    frontend_ip_configuration_name = "LoadBalancerFrontEnd"
+    backend_address_pool_id = "${azurerm_lb_backend_address_pool.backend_pool.id}"
+}
+
+
+
+
+
+resource "azurerm_public_ip" "vm_public_ip" {
+  name                = "${var.environment}-vm-ip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku = "Standard"
+  zones               = ["1"]
+  tags                = var.resource_tags
+}
+
+resource "azurerm_nat_gateway" "nat_gateway" {
+  name                = "${var.environment}-nat-gateway"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku_name                = "Standard"
+  idle_timeout_in_minutes = 10
+  zones                   = ["1"]
+  tags                = var.resource_tags
+}
+
+
+resource "azurerm_subnet_nat_gateway_association" "nat_gateway_assoc" {
+  subnet_id      = azurerm_virtual_network.vnet.subnet[0].id
+  nat_gateway_id = azurerm_nat_gateway.nat_gateway.id
+}
+
+
+resource "azurerm_nat_gateway_public_ip_association" "nat_gateway_pip_assoc" {
+  nat_gateway_id = azurerm_nat_gateway.nat_gateway.id
+  public_ip_address_id   = azurerm_public_ip.vm_public_ip.id
+}
