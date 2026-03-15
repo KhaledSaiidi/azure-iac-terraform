e54e1c327d043e61b373c6d6ccf1fdd35e18068e
KhaledSaiidi <khaled.saiidi@outlook.com>
2026-03-15 12:45:59 +0100

    fix: update storage account name in backend configuration and enhance Terraform plan steps

--- azure-vmss-infra/backend.tf
+++ azure-vmss-infra/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "rg-platform-tfstate"
-    storage_account_name = "platengtfstate"
+    storage_account_name = "platengtfstate1773573416"
     container_name       = "tfstate001"
     key                  = "ks.terraform.tfstate"
   }
